689593a8d0057ade61d8d382daaa187a55d74c17
Vinicius Caique <viniciuscaiquesilva@gmail.com>
2023-06-01 23:46:06 -0300

    config de ambiente

---
+++ src/main/java/com/example/mygreen/MyGreenApplication.java
@@ -0,0 +1,13 @@
+package com.example.mygreen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyGreenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyGreenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mygreen/MyGreenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mygreen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyGreenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
