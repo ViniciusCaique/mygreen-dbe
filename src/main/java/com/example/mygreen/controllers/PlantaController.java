35523cd1afba22d9c8c479ec55c103f427de1103
Vinicius Caique <viniciuscaiquesilva@gmail.com>
2023-06-02 23:43:30 -0300

    consertei

--- src/main/java/com/example/mygreen/controllers/PlantaController.java
+++ src/main/java/com/example/mygreen/controllers/PlantaController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,7 +26,7 @@ import com.example.mygreen.exceptions.RestNotFoundException;
 import com.example.mygreen.models.Planta;
 import com.example.mygreen.repositories.PlantaRepository;
 
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
+// import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import jakarta.validation.Valid;
 
 @RestController
@@ -50,6 +51,8 @@ public class PlantaController {
 
     @PostMapping
     public ResponseEntity<Object> create(@RequestBody @Valid Planta planta){
+        log.info("erro create: "+ planta);
+
         plantaRepository.save(planta);
         
         log.info("erro create: "+ planta);
--- src/main/java/com/example/mygreen/controllers/PlantacaoController.java
+++ src/main/java/com/example/mygreen/controllers/PlantacaoController.java
@@ -17,6 +17,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestBody;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,7 +28,6 @@ import com.example.mygreen.exceptions.RestNotFoundException;
 import com.example.mygreen.models.Plantacao;
 import com.example.mygreen.repositories.PlantacaoRepository;
 
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import jakarta.validation.Valid;
 
 @RestController
